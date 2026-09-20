f14982bc56ac453415edffc47f9039ebe6a8dea2
Alfiya nasrin <alfiya.nazrinva@gmail.com>
2026-09-20 18:32:38 +0530

    fix redis connection string

--- apps/backend/flush_redis.js
+++ apps/backend/flush_redis.js
@@ -2,7 +2,8 @@ import { createClient } from 'redis';
 
 async function main() {
   console.log('Connecting to Redis...');
-  const client = createClient({ url: process.env.REDIS_URL });
+  const url = process.env.REDIS_URL ? process.env.REDIS_URL.replace('cravo_redis', '127.0.0.1') : 'redis://127.0.0.1:6379';
+  const client = createClient({ url });
   
   client.on('error', (err) => console.log('Redis Client Error', err));
   
